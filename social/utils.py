0d2f0519e3c67204f4833c25fc549991ef85b1bd
Hannes Hapke <hannes.hapke@gmail.com>
2016-02-26 13:32:29 -0800

    Fix for sessions in partial auth situations

--- social/utils.py
+++ social/utils.py
@@ -9,6 +9,7 @@ import six
 import requests
 import social
 
+
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
 
@@ -16,6 +17,14 @@ from social.exceptions import AuthCanceled, AuthUnreachableProvider
 from social.p3 import urlparse, urlunparse, urlencode, \
                       parse_qs as battery_parse_qs
 
+# these requirements are needed by the patch for `partial_pipeline_data`
+# from social import utils
+from social.exceptions import InvalidEmail
+
+from django.core import signing
+from django.core.signing import BadSignature
+from django.contrib.sessions.models import Session
+from django.conf import settings
 
 SETTING_PREFIX = 'SOCIAL_AUTH'
 
@@ -160,8 +169,49 @@ def drop_lists(value):
         out[key] = val
     return out
 
+# # DEPRECATED
+# def partial_pipeline_data(backend, user=None, *args, **kwargs):
+#     partial = backend.strategy.session_get('partial_pipeline', None)
+#     if partial:
+#         idx, backend_name, xargs, xkwargs = \
+#             backend.strategy.partial_from_session(partial)
+#         if backend_name == backend.name:
+#             kwargs.setdefault('pipeline_index', idx)
+#             if user:  # don't update user if it's None
+#                 kwargs.setdefault('user', user)
+#             kwargs.setdefault('request', backend.strategy.request_data())
+#             xkwargs.update(kwargs)
+#             return xargs, xkwargs
+#         else:
+#             backend.strategy.clean_partial_pipeline()
+
 
 def partial_pipeline_data(backend, user=None, *args, **kwargs):
+    """
+    Monkey-patch utils.partial_pipeline_data to enable us to retrieve session data by signature key in request.
+    This is necessary to allow users to follow a link in an email to validate their account from a different
+    browser than the one they were using to sign up for the account, or after they've closed/re-opened said
+    browser and potentially flushed their cookies. By adding the session key to a signed base64 encoded signature
+    on the email request, we can retrieve the necessary details from our Django session table.
+    We fetch only the needed details to complete the pipeline authorization process from the session, to prevent
+    nefarious use.
+    """
+    data = backend.strategy.request_data()
+    if 'signature' in data:
+        try:
+            signed_details = signing.loads(data['signature'], key=settings.EMAIL_SECRET_KEY)
+            session = Session.objects.get(pk=signed_details['session_key'])
+        except BadSignature, Session.DoesNotExist:
+            raise InvalidEmail(backend)
+
+        session_details = session.get_decoded()
+        backend.strategy.session_set('email_validation_address', session_details['email_validation_address'])
+        backend.strategy.session_set('next', session_details.get('next'))
+        backend.strategy.session_set('partial_pipeline', session_details['partial_pipeline'])
+        backend.strategy.session_set(backend.name + '_state', session_details.get(backend.name + '_state'))
+        backend.strategy.session_set(backend.name + 'unauthorized_token_name',
+                                     session_details.get(backend.name + 'unauthorized_token_name'))
+
     partial = backend.strategy.session_get('partial_pipeline', None)
     if partial:
         idx, backend_name, xargs, xkwargs = \
@@ -175,7 +225,7 @@ def partial_pipeline_data(backend, user=None, *args, **kwargs):
             return xargs, xkwargs
         else:
             backend.strategy.clean_partial_pipeline()
-
+# utils.partial_pipeline_data = partial_pipeline_data
 
 def build_absolute_uri(host_url, path=None):
     """Build absolute URI with given (optional) path"""
